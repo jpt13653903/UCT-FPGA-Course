c64e749121404b65901bdad49a84748c280d1fb2
John-Philip Taylor <jpt13653903@gmail.com>
2026-07-25 12:56:19 +0200

    03 - Questa

--- Practicals/MyFirstProject.v
+++ Practicals/MyFirstProject.v
@@ -1,7 +1,16 @@
 module MyFirstProject(
+  input  ipClk_50M,
+  input  ipnReset,
+
   input  [9:0]Switch,
-  output [9:0]LED
+  output [9:0]LED,
+
+  output opADXL345_nCS,
+  output opADXL345_SClk,
+  output opADXL345_SDI,
+  input  ipADXL345_SDO
 );
+//------------------------------------------------------------------------------
 
 wire [9:0]Source;
 SourcesAndProbes SourcesAndProbes_inst(
@@ -9,6 +18,44 @@ SourcesAndProbes SourcesAndProbes_inst(
   .probe (Switch)
 );
 assign LED = Switch ^ Source;
+//------------------------------------------------------------------------------
+
+wire [15:0]G_Sensor_X;
+wire [15:0]G_Sensor_Y;
+wire [15:0]G_Sensor_Z;
+
+ADXL345 #(
+  .Clock_kHz(50_000),
+  .Baud_kHz ( 5_000)
+) G_Sensor (
+  .ipClk  (ipClk_50M),
+  .ipReset(~ipnReset),
+
+  .opX    (G_Sensor_X),
+  .opY    (G_Sensor_Y),
+  .opZ    (G_Sensor_Z),
+
+  .opnCS  (opADXL345_nCS ),
+  .opSClk (opADXL345_SClk),
+  .opSDI  (opADXL345_SDI ),
+  .ipSDO  (ipADXL345_SDO )
+);
+//------------------------------------------------------------------------------
+
+wire [1:0]G_Sensor_Select;
+
+altsource_probe #(
+  .instance_id            ("GSNS"),
+  .sld_auto_instance_index("YES"),
+  .probe_width            (16),
+  .source_width           ( 2)
+)SourcesAndProbes_G_Sensor(
+  .source_ena(1'b1),
+  .source    (G_Sensor_Select),
+  .probe     (G_Sensor_Select == 0 ? G_Sensor_X :
+              G_Sensor_Select == 1 ? G_Sensor_Y :
+              G_Sensor_Select == 2 ? G_Sensor_Z : 0)
+);
+//------------------------------------------------------------------------------
 
 endmodule
-
